c35651577db0bedcbbe09d43bc44b62438e1e8ff
Iris Scoffin <iris@scoffin.com>
2021-12-04 20:34:17 +0000

    inital commit

---
+++ desktop/src/com/yorkpirates/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.yorkpirates.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.yorkpirates.game.YorkPirates;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new YorkPirates(), config);
+	}
+}
