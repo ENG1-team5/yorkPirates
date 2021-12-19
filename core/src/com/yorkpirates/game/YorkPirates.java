c27a22338569c523f49ca8e5611c7af2ad49713e
Thom Robinson <tr901@york.ac.uk>
2021-12-19 10:46:30 +0000

    Another Error Fix

--- core/src/com/yorkpirates/game/YorkPirates.java
+++ core/src/com/yorkpirates/game/YorkPirates.java
@@ -1,31 +1,38 @@
 package com.yorkpirates.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class YorkPirates extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+
+	Stage stage;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		// Stage acts as a container for actors, holding the references for them that can be collected
+		// with stage.getActors()
+		stage = new Stage(new ScreenViewport()); // Creates a stage the size of our screen
+		Gdx.input.setInputProcessor(stage); // Wires up the stage as our input processor
+		
+		StaticObject pShip = new PlayerShip("ship.png",100,100);
+		stage.addActor(pShip);
+		stage.setKeyboardFocus(pShip);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		ScreenUtils.clear(1, 1, 1, 1); // Sets background to white
+		stage.act(Gdx.graphics.getDeltaTime()); // Runs the act function for all objects in stage, passes in amount of time since last frame
+		stage.draw();
+	}
+
+	@Override
+	public void dispose(){
+		
 	}
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
 }
